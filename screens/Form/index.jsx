fe72d83c3dcddee7d1ae7a09681e6a148067beb4
Tiago <tiago_s.p@hotmail.com>
2022-12-14 22:24:17 -0300

    Adicionando Tela de Formulario

---
+++ screens/Form/index.jsx
@@ -0,0 +1,83 @@
+import React , {useState }from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import Toast from 'react-native-toast-message';
+import uuid from 'react-native-uuid';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+
+import { styles } from './styles';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+export default function Form() {
+
+  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { getItem, setItem } = useAsyncStorage("@savepass:passwords");
+
+  async function salvarUsuario() {
+
+    try {
+      const id = uuid.v4();
+
+      const newData = {
+        id,
+        name,
+        user,
+        password
+      }
+
+      const response = await getItem();
+      const previousData = response ? JSON.parse(response) : [];
+
+      const data = [...previousData, newData];
+      await setItem(JSON.stringify(data));
+      Toast.show({
+        type: "success",
+        text1: "Cadastrado com sucesso!"
+      })
+    } catch (error) {
+      console.log(error);
+
+      Toast.show({
+        type: "error",
+        text1: "Não foi possível cadastrar."
+      })
+    }
+  }
+
+  return (
+      <View style={styles.content}>
+        <ScrollView>
+        <Text
+          style={styles.text}
+        >Cadastro de Usuário</Text>
+        <View style={styles.form}>
+            <Input
+              label="Nome do Usuário"
+              onChangeText={setName}
+            />
+            <Input
+              label="E-mail"
+              autoCapitalize="none"
+              onChangeText={setUser}
+            />
+            <Input
+              label="Senha"
+              secureTextEntry
+              onChangeText={setPassword}
+            />
+          </View>
+
+          <View style={styles.footer}>
+            <Button
+              title="Salvar"
+              onPress={salvarUsuario}
+            />
+          </View>
+        </ScrollView>
+      </View>
+  );
+}
---
+++ screens/Form/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  content: {
+    backgroundColor: '#EEE',
+    width: '100%',
+    flex: 1
+  },
+  footer: {
+    width: '100%',
+    padding: 24,
+    marginBottom: 24
+  },
+  text: {
+    width: '100%',
+    fontSize: 30,
+    paddingTop: 50,
+    textAlign: 'center',
+    alignSelf: 'center'
+  }
+});
